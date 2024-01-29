10f18ac8568c62ef521b7fa720fedd0f043ed8cb
DH KIM <audegsper@gmail.com>
2024-01-29 12:01:57 +0900

    beta update

---
+++ js/QodpX6ZkmmjbWWp2V0Z7H5U_AN-type-js-k-c0de366ed3dd4cacf9f7894a9b894f6575d39d90-t-1705451089-user-T_js_g3jkb76wb5jjabu6jiejn5_php.js
@@ -0,0 +1,57 @@
+
+function showDiv(){
+	for (var i = 0; i < mainDivEl.length; i++) {
+		animateM( mainDivEl.eq(i) );
+	}
+}
+
+/* 이 상품 어때요? */
+// let prdSwiper;
+function initSwiper(bool, between, view) {
+  
+  return prdSwiper = new Swiper('.prd-slide1 .swiper-container', {
+    spaceBetween: between,		
+    slidesPerView: view,
+    loop: bool,
+	centeredSlides: bool,
+	speed: 1500,					
+	grabCursor: true,
+    autoplay: {
+		delay: 3500,
+		disableOnInteraction: false,
+	  },
+	navigation: {	
+		nextEl: '.prd-slide1 .swiper-button-next',
+		prevEl: '.prd-slide1 .swiper-button-prev',
+	},
+	on: {
+		activeIndexChange: function () {
+		  var display_area = this.realIndex;
+		  console.log(display_area);
+		  $(
+			".gallery_prd .gallery_prd_inner .right_area .right_title > div > ul > li"
+		  )
+			.eq(display_area)
+			.addClass("show");
+		  $(
+			".gallery_prd .gallery_prd_inner .right_area .right_title > div > ul > li"
+		  )
+			.eq(display_area)
+			.siblings()
+			.removeClass("show");
+		},
+	  },
+  });
+}
+
+function mainSizeListener() {
+	if ( $(window).outerWidth() <= 768) {
+		initSwiper(false, 10, 2.3);
+	} else {
+		initSwiper(true, 5, 'auto');
+	}
+}
+
+$(document).ready(function(){
+	mainSizeListener();
+});
